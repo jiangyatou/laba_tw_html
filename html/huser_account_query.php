7bb3a59c204d81492c76b0291f1fef1f36c68318
蒋春梅 <1171801173@qq.com>
2017-05-11 11:40:10 +0800

    订单管理导航修改

--- html/huser_account_query.php
+++ html/huser_account_query.php
@@ -22,10 +22,12 @@
 
 	<div class="s1_tab">
 		<ul class="clearfix">
-			<li><a href="order_list.php">订单管理</a></li>
-			<li class="cur"><a href="account_query.php">账户查询</a></li>
-			<li><a href="chongzhi.php">账户充值</a></li>
-			<li><a href="person_edit.php">用户信息</a></li>
+			<li><a href="">代理会员管理</a></li>
+			<li><a href="">订单管理</a></li>
+			<li class="cur"><a href="">账户查询</a></li>
+			<li><a href="">账户充值</a></li>
+			<li><a href="">用户信息</a></li>
+			<li><a href="huser_huiyuan_add.php">新增会员</a></li>
 		</ul>
 	</div>
 
